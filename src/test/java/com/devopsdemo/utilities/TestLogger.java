620df8637107bec1f1a35b096cf5062dbae440eb
NNUTAN07 <63800096+NNUTAN07@users.noreply.github.com>
2021-04-16 23:40:37 +0530

    Update TestLogger.java

--- src/test/java/com/devopsdemo/utilities/TestLogger.java
+++ src/test/java/com/devopsdemo/utilities/TestLogger.java
@@ -32,7 +32,7 @@ public class TestLogger {
                 }
                 catch (Exception e){
                         s = util.getErrorMessage(e); 
-                }}
+                }}}
 		
                 LOG.info(s);
                 assert(s.contains("/ by zero"));  
